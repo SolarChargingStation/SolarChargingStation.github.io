83b7530f0c038c99253ac15131ef3953ffc38bc7
Nate Durham <nd01679@georgiasouthern.edu>
2020-03-08 10:51:31 -0400

    adjusted pyArduino code... still need to create DC motor code

--- Programs/PiArduinoSunCalc/sun.ino
+++ Programs/PiArduinoSunCalc/sun.ino
@@ -9,8 +9,11 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(4, OUTPUT);   // M1 direction
   pinMode(5, OUTPUT);   // M1 direction
-  pinMode(6, OUTPUT);   // M1 enable
+  pinMode(6, OUTPUT);   // M enable
+  pinMode(7, OUTPUT);   // M2 direction
+  pinMode(8, OUTPUT);   // M2 direction
   pinMode(9, OUTPUT);   // M1 PWM
+  pinMode(10, OUTPUT);  // M2 PWM
   Serial.begin(9600);
   targetRun = true;
 }
@@ -33,29 +36,63 @@ void loop(){
     */
     reading = map(reading, 66, 980, 0, 90);
 
-    // adjusts if reading doesn't match target of linear actuator
-    while(reading < altitude){
-      in();
-    }
-    while(reading > altitude){
-      out();
+    // only adjust if desired altitude is within range
+    if (altitude <= 90 && altitude >= 0) {
+      // adjusts if reading doesn't match target of linear actuator
+      while(reading < altitude){
+        in();
+      }
+      while(reading > altitude){
+        out();
+      }
     }
 
+    // DC Motor
+
+    // end with disabling motors
+    digitalWrite(6, LOW);
   }
 }
 
 ///////////////// FUNCTIONS /////////////////
 
 void in(){
-  digitalWrite(6, HIGH);
   digitalWrite(4, LOW);
   digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
   analogWrite(9, 255);
+  analogWrite(10, 0);
 }
 
 void out(){
-  digitalWrite(6, HIGH);
   digitalWrite(4, HIGH);
   digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
   analogWrite(9, 255);
+  analogWrite(10, 0);
 }
+
+void CW(){
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  analogWrite(9, 0);
+  analogWrite(10, 255);
+}
+
+void CCW(){
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(9, 0);
+  analogWrite(10, 255);
+}
+
