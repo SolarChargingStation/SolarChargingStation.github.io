ef05bd1708f931970ed5703f7e3e44378a85ce18
Nate Durham <nd01679@georgiasouthern.edu>
2020-04-07 10:36:24 -0400

    overhauled python code

--- Programs/arduinoComm/testComm.py
+++ Programs/arduinoComm/testComm.py
@@ -10,33 +10,23 @@ import datetime
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 date = datetime.datetime.now(datetime.timezone.utc)
 
-opCode = int(sys.argv[1])
-altitude = int(get_altitude(32.448, -81.780, date))
-#azimuth = int(get_azimuth(32.448, -81.780, date))
-azimuth = int(90)
+opCode = str(sys.argv[1])
+altitude = str(int(get_altitude(32.448, -81.780, date)))
+azimuth = str(int(get_azimuth(32.448, -81.780, date)))
 
+if (int(altitude) < 10):
+  altitude = ("00"+altitude)
+if (int(altitude)>10 and int(altitude)<100):
+  altitude = ("0"+altitude)
+if (int(azimuth) < 10):
+  azimuth = ("00"+azimuth)
+if (int(altitude)>10 and int(azimuth)<100):
+  azimuth = ("0"+azimuth)
 
+print("opCode:",opCode)
+print("azimuth:",azimuth)
+print("altitude:",altitude)
 
-if __name__ == '__main__':
-
-
-  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-  ser.flush()
-
-  ser.write(b"%d\n" %opCode)
-  time.sleep(2)
-  line = ser.readline().decode('utf-8').rstrip()
-  #line = ser.readline().rstrip()
-  print("opCode:",line)
-  ser.write(b"%d\n" %azimuth)
-  time.sleep(2)
-  line = ser.readline().decode('utf-8').rstrip()
-  #line = ser.readline().rstrip()
-  print("azimuth:",line)
-  ser.write(b"%d\n" %altitude)
-  time.sleep(2)
-  line = ser.readline().decode('utf-8').rstrip()
-  #line = ser.readline().rstrip()
-  print("altitude:",line)
-  line = ser.readline().decode('utf-8').rstrip()
-  time.sleep(2)
+message = opCode+azimuth+altitude+"\r"
+print(message)
+arduino.write(message.encode())
--- Programs/draftCode/draftCode.ino
+++ Programs/draftCode/draftCode.ino
@@ -13,68 +13,70 @@ void setup() {
   pinMode(7, OUTPUT);
 
   Serial.begin(9600);
-  Serial.setTimeout(2000);
 }
 
 void loop() {
-  // gather data
-  if (Serial.available() > 0) {
-    String opCodeStr = Serial.readStringUntil('\n');
-    opCode = opCodeStr.toInt();
-    
-    Serial.println(opCode);
-    delay(1000);
-    String azimuthStr = Serial.readStringUntil('\n');
-    azimuth = azimuthStr.toInt();
-    Serial.println(azimuth);    
-    delay(1000);
-    String altitudeStr = Serial.readStringUntil('\n');
-    altitude = altitudeStr.toInt();    
-    Serial.println(altitude);
+  // gather data  
+  String message(DEC);
+  while(Serial.available()==0);
+  message = Serial.readStringUntil("\r");
+  Serial.println(message);
+  String opCodeStr,azimuthStr,altitudeStr;
+  opCodeStr = opCodeStr+message[0];
+  opCodeStr = opCodeStr+message[1];
+  opCodeStr = opCodeStr+message[2];
+  azimuthStr = azimuthStr+message[3];
+  azimuthStr = azimuthStr+message[4];
+  azimuthStr = azimuthStr+message[5];
+  altitudeStr = altitudeStr+message[6];
+  altitudeStr = altitudeStr+message[7];
+  altitudeStr = altitudeStr+message[8];
+  int opCode = opCodeStr.toInt();
+  int azimuth = azimuthStr.toInt();
+  int altitude = altitudeStr.toInt();
 
-    if (opCode == 1100) {
-      while(encoderPos > 0) {
-        CW();
-      }
-      stopAll();
+  if (opCode == 110) {
+    while(encoderPos > 0) {
+      CW();
     }
+    stopAll();
+  }
 
-    if (opCode == 1200) {
-      //// DC MOTOR
-      // reset encoderPos
-      // translate azimuth into encoder marks, 79.75 encoder signals per degree
-      azimuth = azimuth*79.75;
-      while (encoderPos < azimuth) {
-        Serial.println("Adjusting azimuth");
-        CCW();
-      }
-      stopAll();
-      //// LINEAR ACTUATOR
-      // Fully retracted (60) points up, fully extended (980) points out
-      /*altitude = 90-altitude;
-      if (altitude <= 90 && altitude >= 0) {
+  if (opCode == 120) {
+    //// DC MOTOR
+    // reset encoderPos
+    // translate azimuth into encoder marks, 79.75 encoder signals per degree
+    azimuth = azimuth*79.75;
+    while (encoderPos < azimuth) {
+      Serial.println("Adjusting azimuth");
+      CCW();
+    }
+    stopAll();
+    //// LINEAR ACTUATOR
+    // Fully retracted (60) points up, fully extended (980) points out
+    /*altitude = 90-altitude;
+    if (altitude <= 90 && altitude >= 0) {
+      reading = analogRead(A0);
+      altitude = map(altitude, 0, 90, 60, 980);
+      // has to be within range and has 10 seconds to be completed
+      time1 = millis();
+      while(reading < altitude) {
+        extend();
         reading = analogRead(A0);
-        altitude = map(altitude, 0, 90, 60, 980);
-        // has to be within range and has 10 seconds to be completed
-        time1 = millis();
-        while(reading < altitude) {
-          extend();
-          reading = analogRead(A0);
-          time2 = millis();
-          if(time2-time1 > 10000) {
+        time2 = millis();
+        if(time2-time1 > 10000) {
+        break;
+      }
+    }
+      while(reading > altitude) {
+        retract();
+        reading = analogRead(A0);
+        time2 = millis();
+        if(time2-time1 > 10000) {
           break;
         }
       }
-        while(reading > altitude) {
-          retract();
-          reading = analogRead(A0);
-          time2 = millis();
-          if(time2-time1 > 10000) {
-            break;
-          }
-        }
-      }*/    
-    }
+    }*/    
   }
 }
 
