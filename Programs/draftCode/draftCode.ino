c28a59ebf62b4beb6b690dc505a5696ab4e2b3dd
SolarChargingStation <60202758+SolarChargingStation@users.noreply.github.com>
2020-05-27 10:52:12 -0400

    Add files via upload

--- Programs/draftCode/draftCode.ino
+++ Programs/draftCode/draftCode.ino
@@ -1,133 +1,254 @@
-volatile int encoderPos=0;
-int reading,opCode,azimuth,altitude;
-unsigned long time1,time2;
+#include<SPI.h>
+#include<MFRC522.h>
+
+#define SS_PIN 53
+#define RST_PIN 49
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+int EncoderA = 2;
+int EncoderB = 3;
+int M1C1 = 4;
+int M1C2 = 5;
+int M1PWM = 6;
+int M1E = 7;
+int M2C1 = 8;
+int M2C2 = 9; 
+int M2WM = 10;
+int M2E = 11;
+int M2Pot = A0;
+int greenLed = 30;
+int redLed = 31;
+int resolution = 41.47;
+unsigned long time1, time2;
+
+int reading;
+
+volatile float encoderPos;
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), doEncoderA, RISING);
-  pinMode(3, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(3), doEncoderB, RISING);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+    pinMode(2, INPUT);
+    attachInterrupt(digitalPinToInterrupt(EncoderA), doEncoderA, RISING);
+    pinMode(3, INPUT);
+    attachInterrupt(digitalPinToInterrupt(EncoderB), doEncoderB, RISING);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(30, OUTPUT);
+    pinMode(31, OUTPUT);
+    pinMode(32, INPUT);
+    pinMode(33, INPUT);
+    pinMode(18, INPUT);
+    attachInterrupt(digitalPinToInterrupt(18), RFID, CHANGE);
+    pinMode(19, INPUT);
+    pinMode(53, OUTPUT);
 
-  Serial.begin(115200);
+    SPI.begin();
+    mfrc522.PCD_Init();
+
+    Serial.begin(9600);
 }
 
 void loop() {
-  // gather data  
-  String message(DEC);
-  //Serial.println("<Arduino is ready>");
-  while(Serial.available()==0) {
-    Serial.println("<Arduino is ready>");
-  }
-  message = Serial.readString();
-  Serial.println(message);
-  String opCodeStr,azimuthStr,altitudeStr,encoderStr;
-  opCodeStr = opCodeStr+message[0];
-  opCodeStr = opCodeStr+message[1];
-  opCodeStr = opCodeStr+message[2];
-  azimuthStr = azimuthStr+message[3];
-  azimuthStr = azimuthStr+message[4];
-  azimuthStr = azimuthStr+message[5];
-  altitudeStr = altitudeStr+message[6];
-  altitudeStr = altitudeStr+message[7];
-  altitudeStr = altitudeStr+message[8];
-  
-  int opCode = opCodeStr.toInt();
-  int azimuth = azimuthStr.toInt();
-  int altitude = altitudeStr.toInt();
-
-  if (opCode == 110) {
-    while(encoderPos > 0) {
-      CW();
+    // receive data
+    digitalWrite(53, HIGH);
+    while(Serial.available()==0) {
+        Serial.println("<Arduino is ready>");
+        delay(1000);
     }
-    stopAll();
-  }
-
-  if (opCode == 120) {
-    //// DC MOTOR
-    // reset encoderPos
-    // translate azimuth into encoder marks, 79.75 encoder signals per degree
-    azimuth = azimuth*79.75;
-    while (encoderPos < azimuth) {
-      //Serial.println("Adjusting azimuth");
-      CCW();
+    digitalWrite(53, LOW);
+    String message="";
+    message = Serial.readString();
+    Serial.println(message);
+    String opCodeStr, azimuthStr, altitudeStr;
+    for(int i=0; i<=2; i++) {
+        opCodeStr = opCodeStr + message[i];
     }
-    stopAll();
-    //// LINEAR ACTUATOR
-    // Fully retracted (60) points up, fully extended (980) points out
-    altitude = 90-altitude;
-    if (altitude <= 90 && altitude >= 0) {
-      reading = analogRead(A0);
-      altitude = map(altitude, 0, 90, 60, 980);
-      // has to be within range and has 10 seconds to be completed
-      time1 = millis();
-      while(reading < altitude) {
-        extend();
-        reading = analogRead(A0);
-        time2 = millis();
-        if(time2-time1 > 10000) {
-        break;
-      }
+    for(int i=3; i<=5; i++) {
+        azimuthStr = azimuthStr + message[i];
     }
-      while(reading > altitude) {
-        retract();
-        reading = analogRead(A0);
-        time2 = millis();
-        if(time2-time1 > 10000) {
-          break;
+    for(int i=6; i<=8; i++) {
+        altitudeStr = altitudeStr + message[i];
+    }
+    Serial.println(opCodeStr);
+    Serial.println(azimuthStr);
+    Serial.println(altitudeStr);
+    Serial.println(message);
+    
+    int opCode = opCodeStr.toInt();
+    int azimuth = azimuthStr.toInt();
+    int altitude = altitudeStr.toInt();
+;
+    if (opCode == 110) {
+        while(encoderPos > 0) {
+            CCW();
         }
-      }
+        stopAll();
     }
-  }
-  
-  delay(10000);
+
+    if (opCode == 120) {
+        azimuth = azimuth*resolution;
+        while(encoderPos < azimuth) {
+            CW();
+        }
+        stopAll();
+        delay(1000);
+        altitude = 90-altitude;
+        Serial.println(altitude);
+        reading = analogRead(M2Pot);
+        altitude = map(altitude, 10, 70, 60, 980);
+        Serial.println("Altitude mapped:");
+        Serial.println(altitude);
+        delay(1000);
+        time1 = millis();
+        while(reading < altitude) {
+            extend();
+            reading = analogRead(M2Pot);
+            time2 = millis();
+            if(time2-time1>10000) {
+                break;
+            }
+        }
+        stopAll();
+        while(reading > altitude) {
+            retract();
+            reading = analogRead(M2Pot);
+            time2 = millis();
+            if(time2-time1>10000) {
+                break;
+            }
+        }
+        stopAll();
+    }
+
+   delay(1000);
+   Serial.print(opCodeStr);Serial.print(" ");
+   Serial.print(azimuthStr);Serial.print(" ");
+   Serial.println(altitudeStr);
+   Serial.println("<Return>");
+    
 }
 
-//////// FUNCTIONS ////////
-// ALL MOTORS
-void stopAll() {
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-}
-// M1 - ENCODER
-void doEncoderA() {  
-  encoderPos += (digitalRead(2)==digitalRead(3))?1:0;
-}
-void doEncoderB() {  
-  encoderPos += (digitalRead(2)==digitalRead(3))?-1:0;
-}
-// M1 - DC MOTOR
-void CW() {
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  analogWrite(7, 255);
-}
+// FUNCTIONS
 void CCW() {
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  analogWrite(7, 255);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
 }
-// M2 - LINEAR ACTUATOR
+void CW() {
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+
+}
 void extend() {
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, HIGH);
-  analogWrite(11, 255);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, HIGH);
 }
 void retract() {
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-  analogWrite(11, 255);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, HIGH);
+}
+void stopAll() {
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+}
+void doEncoderA() {
+    encoderPos += (digitalRead(EncoderA)==digitalRead(EncoderB))?0:1;
+}
+void doEncoderB() {
+    encoderPos += (digitalRead(EncoderA)==digitalRead(EncoderB))?0:-1;
+}
+void RFID() {
+    for(int i=0; i<5; i++) {
+        digitalWrite(greenLed, HIGH);
+        digitalWrite(redLed, LOW);
+        delay(50);
+        digitalWrite(greenLed, LOW);
+        digitalWrite(redLed, HIGH);
+        delay(50);
+    }
+    bool state1, state2;
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+        return;
+    }
+    if (!mfrc522.PICC_ReadCardSerial()) {
+        return;
+    }
+    Serial.print("UID tag: ");
+    String content = "";
+    byte letter;
+    for (byte i=0; i<mfrc522.uid.size; i++) {
+        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0":"");
+        Serial.print(mfrc522.uid.uidByte[i], HEX);
+        content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : ""));
+        content.concat(String(mfrc522.uid.uidByte[i], HEX)); 
+    }
+    Serial.println();
+    Serial.println("Message: ");
+    content.toUpperCase();
+    Serial.println(content);
+    if (content == "125C5D42" | content == "A2FAA342") {
+        for(int i=0; i<3; i++) {
+            digitalWrite(greenLed, HIGH);
+            delay(50);
+            digitalWrite(greenLed, LOW);
+            delay(50);
+        }
+        while(1) {
+            state1 = digitalRead(32);
+            state2 = digitalRead(33);
+            while(state1 == 1 && state2 == 0) {
+                CW();
+                digitalWrite(greenLed, HIGH);
+                digitalWrite(redLed, LOW);
+                state1 = digitalRead(32);
+                state2 = digitalRead(33);
+            }
+            stopAll();
+            while(state1 == 0 && state2 == 1) {
+                CCW();
+                digitalWrite(greenLed, LOW);
+                digitalWrite(redLed, HIGH);
+                state1 = digitalRead(32);
+                state2 = digitalRead(33);
+            }
+            digitalWrite(greenLed, LOW);
+            digitalWrite(redLed, LOW);
+            stopAll();
+            if(digitalRead(32) == 1 && digitalRead(33) == 1) {
+                for (int i=0; i<20; i++) {
+                    digitalWrite(greenLed, HIGH);
+                    digitalWrite(redLed, LOW);
+                    delay(50);
+                    digitalWrite(greenLed, LOW);
+                    digitalWrite(redLed, HIGH);
+                    delay(50);
+                    digitalWrite(greenLed, LOW);
+                    digitalWrite(redLed, LOW);
+                }
+                delay(1000);
+                encoderPos = 0;
+                break;
+            }
+        }
+    }
+    
+
 }
